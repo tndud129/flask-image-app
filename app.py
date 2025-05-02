32b62a6c60205dfedab58fbe278484317d83db6d
tndud129 <dbfl129@naver.com>
2025-05-02 15:34:05 +0900

    fix

--- app.py
+++ app.py
@@ -59,6 +59,8 @@ def upload():
     img_io.seek(0)
     return send_file(img_io, mimetype='image/png')
 
-if __name__ != '__main__':
-    app = app
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
